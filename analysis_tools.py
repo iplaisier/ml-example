dcae8c3914bc9fd33b4ee3ee0aecd90fe3fe86f6
Ilse Plaisier <ilse@plaisier.nu>
2024-10-16 12:47:09 +0200

    add analysis tools

---
+++ analysis_tools.py
@@ -0,0 +1,20 @@
+class EarlyStopping:
+    def __init__(self, patience=2, delta=0):
+        self.patience = patience
+        self.delta = delta
+        self.best_score = None
+        self.early_stop = False
+        self.counter = 0
+        self.best_model_state = None
+
+    def check(self, val_loss, model):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.counter = 0
